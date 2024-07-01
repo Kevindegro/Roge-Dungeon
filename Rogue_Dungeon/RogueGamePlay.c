776919c87b8c36b7b209eeb74b1bcd2dfe417b90
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-07-01 16:22:46 -0300

    Update RogueGamePlay.c

--- Rogue_Dungeon/RogueGamePlay.c
+++ Rogue_Dungeon/RogueGamePlay.c
@@ -36,9 +36,12 @@ void mostrarmapa(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorr
                 printf("%c ", '?');
             }  
             else if(mazmorra[i][j] == '@'){
-                if(player.brujula){ 
-                    printf(RED"@ "RESET); 
+                if(player.brujula){ //Cuando tengas la brujula, te muestra tu ubicacion en el mapa
+                    printf(YELLOW"@ "RESET); 
                 }
+                    else if(mazmorra[i][j + 1] == '#' &&  mazmorra[i][j - 1] == '#'){
+                        printf(". "); 
+                    }
                 else { 
                     printf(". "); 
                 }
@@ -51,23 +54,23 @@ void mostrarmapa(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorr
     }
     printf("-----------------------------------------------------------------------------------------------------\n");
     printf("Presione enter para seguir moviendose\n");
-    while (getchar() != '\n');
+    while (getchar() != '\n'); // \n = enter Cuando se presione enter va a seguir como estaba todo
     limpiar_consola();
 }
 
 //-------------------------------------------------------------------------------------------------------
 
-void menu(struct caracteristicas jugador){
-    limpiar_consola();
-    cargarImagen("ROGUE/Txt/sProtagonist.txt");
-    printf("\nVida: %d\n", jugador.vida);
-    printf("Ataque(maximo): %d\n", jugador.ataque);
-    printf("Resistencia: %d\n", jugador.resistencia);
-    printf("Experiencia: %d/%d\n", jugador.experiencia, jugador.experienciaSubirNivel);
-    printf("Curaciones: %d💉\n", jugador.curaciones);
-    printf("%s\n", jugador.brujula ? "🧭" : " ");
-    printf("Presione enter para seguir moviendose\n");
-    while (getchar() != '\n');
+void menu(struct caracteristicas jugador){ //Un menu junto a un struct para ver tus caracteristicas
+    limpiar_consola(); //Esto limpia todo
+    cargarImagen("ROGUE/Txt/sProtagonist.txt"); //Esto carga tu imagen
+    printf("\nVida: %d\n", jugador.vida); //Eso muestra tu vida mediante un struct de caracteristicas
+    printf("Ataque(maximo): %d\n", jugador.ataque); //Esto muestra tu ataque mediante un struct de car-
+    printf("Resistencia: %d\n", jugador.resistencia);//Esto muestra tu resistencia mediante un struct de car-
+    printf("Experiencia: %d/%d\n", jugador.experiencia, jugador.experienciaSubirNivel); //Esto muestra tu exp y falta de exp mediante un struct de car-
+    printf("Curaciones: %d💉\n", jugador.curaciones);//Esto muestra tus curaciones mediante un struct de car-
+    printf("%s\n", jugador.brujula ? "🧭" : " "); //Muestra si posees la brujula
+    printf("Presione enter para seguir moviendose\n"); 
+    while (getchar() != '\n'); //Lo mismo del enter otra vez
     limpiar_consola();
 }
 
@@ -76,10 +79,10 @@ void menu(struct caracteristicas jugador){
 int movimientos(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorra], char jugador[], struct caracteristicas player) {
     srand(time(NULL));
     //Asigna todos los ints que necesita la fucion movimientos
-    int fila = player.fila;
-    int columna = player.columna;
+    int fila = player.fila; //estos dos ints son para las fila y columna, estos sirven para exportarlos
+    int columna = player.columna; //desde otra funcion a aca
     int nuevafila = fila;
-    int nuevacolumna = columna;
+    int nuevacolumna = columna; //eae
     char movimiento;
     int moves = 1;
     char original = '.';
@@ -87,7 +90,7 @@ int movimientos(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorra
     imprimirMazmorra(tamanomazmorra, mazmorra, fila, columna, player); //Imprime la mazmorra
 
     struct termios old_tio; //Llama a la funcion para que no se necesite presionar enter
-    desactivarModoCanonico(&old_tio);
+    desactivarModoCanonico(&old_tio); //Esto es para que no se necesite presionar enter
     while (1) {
         while(moves == 1){
             movimiento = getchar();  // Lee un carácter sin necesidad de presionar Enter
@@ -95,24 +98,24 @@ int movimientos(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorra
 
             //Los movimientos:
             if (movimiento == 'w' || movimiento == 'W') {
-                nuevafila = fila - 1;
-                nuevacolumna = columna;
+                nuevafila = fila - 1; //Con W se mueve hacia arriba //Se le resta uno a fila y 
+                nuevacolumna = columna; //Guarda el valor nuevo
             } else if (movimiento == 'a' || movimiento == 'A') {
-                nuevafila = fila;
-                nuevacolumna = columna - 1;
+                nuevafila = fila; //Con A se mueve hacia la izquierda
+                nuevacolumna = columna - 1; //Guarda el valor nuevo
             } else if (movimiento == 's' || movimiento == 'S') {
-                nuevafila = fila + 1;
-                nuevacolumna = columna;
+                nuevafila = fila + 1; //Con S se mueve hacia abajo
+                nuevacolumna = columna;//Guarda el valor nuevo
             } else if (movimiento == 'd' || movimiento == 'D') {
-                nuevafila = fila;
-                nuevacolumna = columna + 1;
+                nuevafila = fila; //Con D se mueve hacia la derecha
+                nuevacolumna = columna + 1; //Esto 
             } 
             else if ((movimiento == 'c' || movimiento == 'C') && player.curaciones >= 1) {
-                player.vida += 7;
-                player.curaciones--;
+                player.vida += 7; //Si apretas c, va a curarte y va a restar a curaciones
+                player.curaciones--; //Que son las jeringas
             }
             else if(movimiento == 'm' || movimiento == 'M'){ //Este es para elegir el mapa
-                //LLam a la funcion de mapa
+                //LLama la funcion de mapa
                 mostrarmapa(tamanomazmorra, mazmorra, jugador, player);
             }
             else if(movimiento == 'q' || movimiento == 'Q'){ //Este es para elegir el menu
@@ -127,16 +130,16 @@ int movimientos(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorra
                 continue;
             }
             if (mazmorra[nuevafila][nuevacolumna] == 'V') {
-                player.curaciones++;
+                player.curaciones++; //Si el lugar en donde estamos es igual a V, suma las curaciones
             }
             if (mazmorra[nuevafila][nuevacolumna] == 'B') {
-                player.brujula = true;
+                player.brujula = true; //Si el lugar donde estamos es igual a B, conseguimos la brujula
             }
             if (mazmorra[nuevafila][nuevacolumna] == ' ') {
                 limpiar_consola();
                 imprimirMazmorra(tamanomazmorra, mazmorra, fila, columna, player);
-                puts("Movimiento fuera de los límites.");
-                continue;
+                puts("Movimiento fuera de los límites."); //Es un loop para que no se rompa el codigo
+                continue; //Al intentar salir fuera de los limites (' ')
                 //Si estas en el borde de la matriz y te moves hacia un lugar que no vale nada
                 //Se activa este if
             }
@@ -148,6 +151,7 @@ int movimientos(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorra
             player.columna = columna;
             limpiar_consola();
             imprimirMazmorra(tamanomazmorra, mazmorra, fila, columna, player); //Esta parte de todos los if junto a esta variable
+            //-------------------------------------------------------------
             //Hacen que si estas parado a la derecha, izquierda, arriba o abajo (por eso mismo
             //los +1 de fila y columna) que o detecte e inicie el modo de pelea
             if (mazmorra[fila][columna + 1] == 'C' || mazmorra[fila][columna + 1] == 'W' || mazmorra[fila][columna + 1] == 'D') {
@@ -164,7 +168,8 @@ int movimientos(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorra
             }
                         
             if ((mazmorra[fila][columna + 1] == 'C' || mazmorra[fila][columna + 1] == 'W' || mazmorra[fila][columna + 1] == 'D') || (mazmorra[fila + 1][columna] == 'C' || mazmorra[fila + 1][columna] == 'W' || mazmorra[fila + 1][columna] == 'D') || (mazmorra[fila][columna - 1] == 'C' || mazmorra[fila][columna - 1] == 'W'|| mazmorra[fila][columna - 1] == 'D') || (mazmorra[fila - 1][columna] == 'C' || mazmorra[fila - 1][columna] == 'W' || mazmorra[fila - 1][columna] == 'D' )) {
-                    
+                    //-------------------------------------------------------------
+                
                 moves = 0;
                 printf(RED"Aparecio un enemigo\n"RESET);
                 for(int i = 0; i < 5; i++){
@@ -180,7 +185,11 @@ int movimientos(int tamanomazmorra, char mazmorra[tamanomazmorra][tamanomazmorra
                     printf("└──────────────────────────────────┘\n");
                     usleep(200000); // Pausa de 0.2 segundos
                     limpiar_consola();
+                   
                 }
+                cargarImagen("ROGUE/Txt/entradaPelea.txt");
+                printf(RED"\n\nPresione enter para pelear\n"RESET);
+                while(getchar() != '\n');
                 pelea(player, tamanomazmorra, mazmorra, jugador);
             }
         }
